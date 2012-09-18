04560aa320e4ac82aec7eff709a5eb92557d2c6b
Hunter Eskew <hunter@riotapps.com>
2012-09-17 20:43:52 -0400

    commented out logging experiment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  		Rails.logger.info("application controller I18n.locale: #{I18n.locale}")
-		 I18n.reload!
+  	#	Rails.logger.info("application controller I18n.locale: #{I18n.locale}")
+	#	 I18n.reload!
 	#	I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-	 Rails.logger.info("application controller I18n.locale: #{I18n.locale}")
+	# Rails.logger.info("application controller I18n.locale: #{I18n.locale}")
 
 	
 	#def extract_locale_from_accept_language_header
