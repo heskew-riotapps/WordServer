5040407e7525b20f793f841abc03c6dc27e7be76
Hunter Eskew <hunter@riotapps.com>
2013-02-21 23:29:46 -0500

    nil check corrected

--- app/services/player_service.rb
+++ app/services/player_service.rb
@@ -134,7 +134,7 @@ class PlayerService
 				#if password fails, send login failed error to client
 				#error codes via http or just error strings??
 				#if @player.authenticate_with_new_token(params[:password])
-				if !@player.fb.nil? 
+				if @player.fb.nil? 
 					@player.fb = ""
 				end
 				
